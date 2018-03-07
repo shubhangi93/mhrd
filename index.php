3f3bc19ad3a62fed48a2f73c793cc716504744b2
shubhangi93 <37019413+shubhangi93@users.noreply.github.com>
2018-03-06 21:58:19 -1100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,154 @@
+<?php
+
+error_reporting(-1);
+
+
+require_once "inc/metaheader.php";
+meta("Ministry of Human Resource Development","The default port used by Apache is 80.");
+
+navbar();
+?>
+
+<br>
+<br>
+<br>
+<br>
+<div class="slider-container">
+    <div class="slider-control left inactive"></div>
+    <div class="slider-control right"></div>
+    <ul class="slider-pagi"></ul>
+    <div class="slider">
+        <div class="slide slide-0 active">
+            <div class="slide__bg"></div>
+            <div class="slide__content">
+                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+                </svg>
+                <div class="slide__text">
+                    <h2 class="slide__text-heading">Slide 1</h2>
+                    <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="slide slide-1 ">
+            <div class="slide__bg"></div>
+            <div class="slide__content">
+                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+                </svg>
+                <div class="slide__text">
+                    <h2 class="slide__text-heading">Project name 2</h2>
+                    <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+                    <a class="slide__text-link">Project link</a>
+                </div>
+            </div>
+        </div>
+        <div class="slide slide-2">
+            <div class="slide__bg"></div>
+            <div class="slide__content">
+                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+                </svg>
+                <div class="slide__text">
+                    <h2 class="slide__text-heading">Project name 3</h2>
+                    <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+                    <a class="slide__text-link">Project link</a>
+                </div>
+            </div>
+        </div>
+        <div class="slide slide-3">
+            <div class="slide__bg"></div>
+            <div class="slide__content">
+                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+                </svg>
+                <div class="slide__text">
+                    <h2 class="slide__text-heading">Project name 4</h2>
+                    <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+                    <a class="slide__text-link">Project link</a>
+                </div>
+            </div>
+        </div>
+        <div class="slide slide-4">
+            <div class="slide__bg"></div>
+            <div class="slide__content">
+                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+                </svg>
+                <div class="slide__text">
+                    <h2 class="slide__text-heading">New Slide</h2>
+                    <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="about">
+    <div class="container">
+        <div class="col-sm-12 ">
+            <div class="col-sm-6 inner-about">
+                <center>
+                    <h1>ABOUT</h1>
+                </center>
+                <br>
+                <p>
+                    Writing can gives them an enormous confidence boost, as well as help them consolidate their literacy learning by putting their phonics, grammar and reading skills into practice.
+                </p>
+                <p>Writing can gives them an enormous confidence boost, as well as help them consolidate their literacy learning by putting their phonics, grammar and reading skills into practice.</p>
+
+            </div>
+            <div class="col-sm-6">
+                <img src="" alt="about" class="img-responsive">
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="services">
+    <div class="container">
+        <div class="col-sm-12 ">
+            <div class="col-sm-6">
+                <img src="" alt="services" class="img-responsive">
+            </div>
+
+            <div class="col-sm-6 inner-services">
+                <center>
+                    <h1>SERVICES</h1>
+                </center>
+                <br>
+                <p>
+                    Writing can gives them an enormous confidence boost, as well as help them consolidate their literacy learning by putting their phonics, grammar and reading skills into practice.
+                </p>
+                <p>Writing can gives them an enormous confidence boost, as well as help them consolidate their literacy learning by putting their phonics, grammar and reading skills into practice.</p>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="contact">
+    <div class="container">
+        <div class="col-sm-12 ">
+            <div class="col-sm-6 inner-about">
+                <center>
+                    <h1>CONTACT</h1>
+                </center>
+                <br>
+                <p>
+                    Writing can gives them an enormous confidence boost, as well as help them consolidate their literacy learning by putting their phonics, grammar and reading skills into practice.
+                </p>
+                <p>Writing can gives them an enormous confidence boost, as well as help them consolidate their literacy learning by putting their phonics, grammar and reading skills into practice.</p>
+
+            </div>
+            <div class="col-sm-6">
+                <img src="" alt="contact" class="img-responsive">
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php
+footer();
+?>
